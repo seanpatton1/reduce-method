9fa35ab86dfef1e00e4b5fed30e3aeaabd9abbfa
Sean Patton <seanpatton1@outlook.com>
2024-08-19 20:41:03 +0000

    reduce

--- reduce.js
+++ reduce.js
@@ -6,7 +6,20 @@
 
 
 // Summing an array of numbers:
+// const nums = [0, 1, 2, 3, 4];
+// let sum = nums.reduce((acc, curr) => acc + curr, 0);
+// console.log(sum);
 
+// const nums = [0, 1, 2, 3, 4];
+// let sum = nums.reduce((acc, curr) => {
+//   console.log(
+//     "Accumulator: ", acc,
+//     "Current Value: ", curr,
+//     "Total: ", acc + curr
+//   )
+//   return acc + curr;
+// });
+// console.log(sum);
 
 const teamMembers = [
   {
@@ -28,10 +41,29 @@ const teamMembers = [
     name: 'Kelly',
     profession: 'Designer',
     yrsExperience: 3
+  },
+  {
+    name: 'James',
+    profession: 'Manager',
+    yrsExperience: 3
   }
 ];
 
 // Totaling a specific object property
 
+let totalExperience = teamMembers.reduce((acc, curr) => acc + curr.yrsExperience, 0);
+console.log(totalExperience);
+
 
 // Grouping by a property, and totaling it too
+let experienceByprofession = teamMembers.reduce((acc, curr) => {
+  let key = curr.profession;
+  if (!acc[key]) {
+    acc[key] = curr.yrsExperience;
+  } else {
+    acc[key] += curr.yrsExperience;
+  }
+  return acc;
+}, {});
+
+console.log(experienceByprofession);
